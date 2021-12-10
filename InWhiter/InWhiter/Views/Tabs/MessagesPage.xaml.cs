a268256388e7e9ae7f3a849fc1e3f29592c61744
Ilya <hairais@mail.ru>
2021-12-10 19:45:54 +0700

    Initial commit

---
+++ InWhiter/InWhiter/Views/Tabs/MessagesPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InWhiter.Views.Tabs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessagesPage : Page
+    {
+        public MessagesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InWhiter/InWhiter/Views/Tabs/ProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InWhiter.Views.Tabs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : Page
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
